466824a89e7bb77fa18bed7806decfe8ad7e3b79
piyush lale <96431711+piyushpy63@users.noreply.github.com>
2025-12-07 12:09:09 +0530

    CodeCommit

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello():
+    hostname=socket.gethostname()
+    return f"hello from POD: {hostname}\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
